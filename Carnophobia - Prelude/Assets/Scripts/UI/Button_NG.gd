d6cc2831d14fe43ff1d007f063da7feb4cfac234
EvergreenGameStudios <153794424+EvergreenGameStudios@users.noreply.github.com>
2024-02-07 22:34:23 -0600

    script changes
    added devroom variable, if set to 1 then "new game" will send you to the devroom. (currently only functional "level" in the game.)

--- Carnophobia - Prelude/Assets/Scripts/UI/Button_NG.gd
+++ Carnophobia - Prelude/Assets/Scripts/UI/Button_NG.gd
@@ -1,6 +1,6 @@
 extends Button
 
-
+@export var devroom = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,4 +12,8 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Assets/Levels/level0-1.tscn") 
+	get_tree().change_scene_to_file("res://Assets/Levels/level0-1.tscn")
+	
+	if devroom == 1:
+		get_tree().change_scene_to_file("res://Assets/Levels/level_none_DEVROOM.tscn")
+	
